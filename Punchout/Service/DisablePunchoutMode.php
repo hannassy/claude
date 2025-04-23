1e99f98a9f5b007531c172866b8d3d519d79c2a0
Pavel <hannpavel@gmail.com>
2025-04-23 14:43:04 +0300

    fixed disable punchout mode class

--- Punchout/Service/DisablePunchoutMode.php
+++ Punchout/Service/DisablePunchoutMode.php
@@ -3,17 +3,17 @@ declare(strict_types=1);
 
 namespace Tirehub\Punchout\Service;
 
-use Tirehub\Punchout\Api\EnablePunchoutModeInterface;
+use Tirehub\Punchout\Api\DisablePunchoutModeInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 
-class DisablePunchoutMode implements EnablePunchoutModeInterface
+class DisablePunchoutMode implements DisablePunchoutModeInterface
 {
     public function __construct(
         private readonly CustomerSession $customerSession
     ) {
     }
 
-    public function execute(string $buyerCookie): void
+    public function execute(): void
     {
         $this->customerSession->unsetData('is_punchout_mode');
         $this->customerSession->unsetData('buyer_cookie');
