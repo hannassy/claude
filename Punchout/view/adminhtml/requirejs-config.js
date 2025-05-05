b52e8d0cc5c8c3ed19a4539e5482dd3b827e331b
Pavel <hannpavel@gmail.com>
2025-05-05 10:50:38 +0300

    sync

--- Punchout/view/adminhtml/requirejs-config.js
+++ Punchout/view/adminhtml/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            'Tirehub_Punchout/js/grid/cxml-content': 'Tirehub_Punchout/js/grid/cxml-content'
+            'Tirehub_Punchout/js/grid/cxml-content': 'Tirehub_Punchout/js/grid/cxml-content',
+            'Tirehub_Punchout/js/grid/items-content': 'Tirehub_Punchout/js/grid/items-content'
         }
     },
     paths: {
