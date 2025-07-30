51ae03a0ba6f483c1a87e6dd5a7ff21294a37ac6
Pavlo Potapenko <ppota@softserveinc.com>
2025-07-30 13:03:49 +0300

    NetworkTransfer

--- TransferNetwork/view/adminhtml/requirejs-config.js
+++ TransferNetwork/view/adminhtml/requirejs-config.js
@@ -4,6 +4,12 @@ var config = {
             'Magento_Ui/js/form/form': {
                 'Tirehub_TransferNetwork/js/form/form-mixin': false
             }
+        },
+        map: {
+            '*': {
+                'Tirehub_TransferNetwork/js/form/import-form': 'Tirehub_TransferNetwork/js/form/import-form',
+                'Tirehub_TransferNetwork/js/grid/listing': 'Tirehub_TransferNetwork/js/grid/listing'
+            }
         }
     }
 };
